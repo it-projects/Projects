c53d4d7c7ab7cec1e9cc8a978c015656add299f0
it-projects <radimcieslar@seznam.cz>
2017-02-06 02:41:01 +0100

    Rest-Index

---
+++ Rest/Rest/Models/Document.cs
@@ -0,0 +1,18 @@
+﻿namespace Rest.Models
+{
+    public class Document
+    {
+        public string HeadTitle { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+        public string Company { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Ico { get; set; }
+    }
+}
