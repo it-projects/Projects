ad558789925e1766055f25ceba4eaa6e2d8c5229
it-projects <radimcieslar@seznam.cz>
2017-02-08 17:06:19 +0100

    Revert "Login"
    This reverts commit 71bdee5851b962e66a68d183ddbb739bcd10a9b5.

--- Rest/Rest/Controllers/LoginController.cs
+++ Rest/Rest/Controllers/LoginController.cs
@@ -1,66 +1,35 @@
 ﻿using Rest.Models;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
-using System.Web.UI.WebControls;
-using XML_Reader;
 
 namespace Rest.Controllers
 {
     public class LoginController : Controller
     {
+        Login login = new Login();
+        HomeController home = new HomeController();
+
         
-        Document doc = new Document();
-        ReadClass reader = new ReadClass();
-
-        private void Login()
+        private Collection<string> Login_Read()
         {
-            Collection<string> pom = reader.Login_reader();
+            home.pom = home.read.Login_reader();
 
-            doc.LoginName = pom[0];
-            doc.LoginPassword = pom[1];
-            doc.LoginEmail = pom[2];
-        }
-
-        private void Document()
-        {
-            Collection<string> pom = reader.Login_reader();
-
-            pom = reader.Text_reader();
-
-            doc.Name = pom[3];
-            doc.Adress = pom[4];
-            doc.City = pom[5];
-            doc.PostCode = pom[6];
-            doc.Company = pom[7];
-            doc.Phone = pom[8];
-            doc.Email = pom[9];
-            doc.Ico = pom[10];
-        }
-        
-        [HttpPost]
-        public ActionResult Log_in(Document document)
-        {
-            Login();
-            Document();
-
-            if (doc.LoginName == document.LoginName && doc.LoginPassword == document.LoginPassword)
-            {            
-                return View("Form", doc);
-            }
-            return View("_Login", doc);
+            return home.pom;
         }
 
         // GET: Login
         public ActionResult _Login()
-        {          
-             Document();     
-             return View(doc);            
+        {
+            return View(home.Doc_read());
         }
 
         public ActionResult Form()
         {
-            Document();
-            return View(doc);
+            return View(home.Doc_read());
         }
     }
 }
--- Rest/Rest/Models/Document.cs
+++ Rest/Rest/Models/Document.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-
-namespace Rest.Models
+﻿namespace Rest.Models
 {
     public class Document
     {
@@ -17,15 +14,5 @@ namespace Rest.Models
         public string Phone { get; set; }
         public string Email { get; set; }
         public string Ico { get; set; }
-
-        [Required(ErrorMessage ="Pole je povinné!")]
-        [MaxLength(6,ErrorMessage ="Špatné jméno!")]
-        public string LoginName { get; set; }
-
-        [Required(ErrorMessage ="Pole je povinné!")]
-        [MaxLength(6, ErrorMessage ="Špatné heslo!")]
-        public string LoginPassword { get; set; }
-
-        public string LoginEmail { get; set; }
     }
 }
