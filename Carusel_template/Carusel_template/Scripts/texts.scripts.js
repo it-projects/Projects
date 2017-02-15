785aaa72131b783f817489514f8f06b1fe7d25da
it-projects <radimcieslar@seznam.cz>
2017-02-16 00:25:51 +0100

    Kontakt, gallerie

--- Carusel_template/Carusel_template/Scripts/texts.scripts.js
+++ Carusel_template/Carusel_template/Scripts/texts.scripts.js
@@ -25,3 +25,28 @@ $("[class^=text]").hide();
             $(".text4").show(500);
         });
     });
+
+//CAROUSEL--------------------------------------------------------------
+    jQuery(document).ready(function ($) {
+
+        $('#myCarousel').carousel({
+            interval: 5000
+        });
+
+        //Handles the carousel thumbnails
+        $('[id^=carousel-selector-]').click(function () {
+            var id_selector = $(this).attr("id");
+            try {
+                var id = /-(\d+)$/.exec(id_selector)[1];
+                console.log(id_selector, id);
+                jQuery('#myCarousel').carousel(parseInt(id));
+            } catch (e) {
+                console.log('Regex failed!', e);
+            }
+        });
+        // When the carousel slides, auto update the text
+        $('#myCarousel').on('slid.bs.carousel', function (e) {
+            var id = $('.item.active').data('slide-number');
+            $('#carousel-text').html($('#slide-content-' + id).html());
+        });
+    });
